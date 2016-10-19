22aaa29a5618baed1239400cdae18377841b373d
Boleslav Bobcik <bbobcik@gmail.com>
2016-10-19 09:44:18 +0200

    Added tasks that assemble source and Javadoc JARs

--- src/main/java/cz/auderis/tools/gradle/TestSupportPlugin.java
+++ src/main/java/cz/auderis/tools/gradle/TestSupportPlugin.java
@@ -9,12 +9,15 @@ import org.gradle.api.internal.ConventionMapping;
 import org.gradle.api.internal.IConventionAware;
 import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact;
 import org.gradle.api.internal.java.JavaLibrary;
+import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet;
 import org.gradle.api.plugins.BasePlugin;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.bundling.Jar;
+import org.gradle.api.tasks.javadoc.Javadoc;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
 
 import java.io.File;
@@ -33,14 +36,16 @@ public class TestSupportPlugin implements Plugin<Project> {
     public static final String SUPPORT_CONFIGURATION_NAME = "testSupport";
     public static final String SUPPORT_COMPILE_CLASSPATH_CONFIGURATION_NAME = "testSupportCompileClasspath";
     public static final String SUPPORT_RUNTIME_CONFIGURATION_NAME = "testSupportRuntime";
+    public static final String SUPPORT_JAVADOC_TASK_NAME = SUPPORT_CONFIGURATION_NAME + "Javadoc";
     public static final String SUPPORT_JAR_TASK_NAME = SUPPORT_CONFIGURATION_NAME + "Jar";
-
+    public static final String SUPPORT_SOURCE_JAR_TASK_NAME = SUPPORT_CONFIGURATION_NAME + "SourceJar";
+    public static final String SUPPORT_JAVADOC_JAR_TASK_NAME = SUPPORT_JAVADOC_TASK_NAME + "Jar";
 
     public static final String SUPPORT_SOURCE_BASE = "src/test-support";
     public static final String SUPPORT_JAVA_SOURCES = SUPPORT_SOURCE_BASE + "/java";
     public static final String SUPPORT_RESOURCES = SUPPORT_SOURCE_BASE + "/resources";
 
-    public static final String SUPPORT_JAR_CLASSIFIER = "test_support";
+    public static final String SUPPORT_JAR_APPENDIX = "test_support";
 
 
     @Override
@@ -50,6 +55,7 @@ public class TestSupportPlugin implements Plugin<Project> {
 
         configureSourceSet(javaConvention);
         configureConfigurations(javaConvention);
+        configureJavadoc(javaConvention);
         configureArchives(javaConvention);
     }
 
@@ -94,6 +100,8 @@ public class TestSupportPlugin implements Plugin<Project> {
         final Project project = pluginConvention.getProject();
         final ConfigurationContainer configurations = project.getConfigurations();
         final Configuration supportConfiguration = configurations.create(SUPPORT_CONFIGURATION_NAME);
+        supportConfiguration.setDescription("Configuration for test support artifacts");
+        supportConfiguration.setVisible(false);
 
         // Make the new configuration extend a configuration created by JavaBasePlugin
         final SourceSet supportSet = pluginConvention.getSourceSets().getByName(SUPPORT_SOURCE_SET_NAME);
@@ -108,27 +116,74 @@ public class TestSupportPlugin implements Plugin<Project> {
         supportRuntimeConfiguration.extendsFrom(runtimeConfiguration);
     }
 
+    private void configureJavadoc(JavaPluginConvention pluginConvention) {
+        final Project project = pluginConvention.getProject();
+        final SourceSet supportSet = pluginConvention.getSourceSets().getByName(SUPPORT_SOURCE_SET_NAME);
+        final Javadoc supportJavadocTask = project.getTasks().create(SUPPORT_JAVADOC_TASK_NAME, Javadoc.class);
+        supportJavadocTask.setDescription("Generates Javadoc API documentation for the test support source code");
+        supportJavadocTask.setGroup(JavaBasePlugin.DOCUMENTATION_GROUP);
+        supportJavadocTask.setClasspath(supportSet.getOutput().plus(supportSet.getCompileClasspath()));
+        supportJavadocTask.source(supportSet.getAllJava());
+        supportJavadocTask.source(pluginConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).getAllJava());
+        supportJavadocTask.setDestinationDir(new File(pluginConvention.getDocsDir(), "test-support-javadoc"));
+        final String fullProjectName;
+        final Object version = project.getVersion();
+        if (Project.DEFAULT_VERSION.equals(version)) {
+            fullProjectName = project.getName();
+        } else {
+            fullProjectName = project.getName() + ", version " + version;
+        }
+        supportJavadocTask.setTitle("Test support API for " + fullProjectName);
+    }
+
     private void configureArchives(JavaPluginConvention pluginConvention) {
         final Project project = pluginConvention.getProject();
+        final SourceSet supportSet = pluginConvention.getSourceSets().getByName(SUPPORT_SOURCE_SET_NAME);
+
+        // Define task for main JAR
         final Jar supportJarTask = project.getTasks().create(SUPPORT_JAR_TASK_NAME, Jar.class);
         supportJarTask.setDescription("Assembles a jar containing test support classes");
         supportJarTask.setGroup(BasePlugin.BUILD_GROUP);
-        supportJarTask.setClassifier(SUPPORT_JAR_CLASSIFIER);
-
-        // Define JAR contents
-        final SourceSet supportSet = pluginConvention.getSourceSets().getByName(SUPPORT_SOURCE_SET_NAME);
+        supportJarTask.setAppendix(SUPPORT_JAR_APPENDIX);
         supportJarTask.from(supportSet.getOutput());
+
+        // Define task for source JAR
+        final Jar supportSourceJarTask = project.getTasks().create(SUPPORT_SOURCE_JAR_TASK_NAME, Jar.class);
+        supportSourceJarTask.setDescription("Assembles a jar containing test support sources");
+        supportSourceJarTask.setGroup(BasePlugin.BUILD_GROUP);
+        supportSourceJarTask.setAppendix(SUPPORT_JAR_APPENDIX);
+        supportSourceJarTask.setClassifier("source");
+        supportSourceJarTask.from(supportSet.getAllJava());
+
+        // Define task for Javadoc JAR
+        final Jar supportJavadocJarTask = project.getTasks().create(SUPPORT_JAVADOC_JAR_TASK_NAME, Jar.class);
+        supportJavadocJarTask.setDescription("Assembles a jar containing test support API documentation");
+        supportJavadocJarTask.setGroup(JavaBasePlugin.DOCUMENTATION_GROUP);
+        supportJavadocJarTask.setAppendix(SUPPORT_JAR_APPENDIX);
+        supportJavadocJarTask.setClassifier("javadoc");
+        final Task javadocTask = project.getTasks().getByName(SUPPORT_JAVADOC_TASK_NAME);
+        supportJavadocJarTask.from(javadocTask);
 
         // Set up publishing
         final ArchivePublishArtifact supportArtifact = new ArchivePublishArtifact(supportJarTask);
+        final ArchivePublishArtifact supportSourceArtifact = new ArchivePublishArtifact(supportSourceJarTask);
+        final ArchivePublishArtifact supportJavadocArtifact = new ArchivePublishArtifact(supportJavadocJarTask);
         final Configuration supportConfiguration = project.getConfigurations().getByName(SUPPORT_CONFIGURATION_NAME);
         supportConfiguration.getArtifacts().add(supportArtifact);
+        supportConfiguration.getArtifacts().add(supportSourceArtifact);
+        supportConfiguration.getArtifacts().add(supportJavadocArtifact);
+
+        final DefaultArtifactPublicationSet publicationSet = project.getExtensions().getByType(DefaultArtifactPublicationSet.class);
+        publicationSet.addCandidate(supportArtifact);
+        publicationSet.addCandidate(supportSourceArtifact);
+        publicationSet.addCandidate(supportJavadocArtifact);
+
         final JavaLibrary supportLibrary = new JavaLibrary(supportArtifact, supportConfiguration.getAllDependencies());
         project.getComponents().add(supportLibrary);
 
         // Make a prerequisite for 'assemble' task
         final Task assembleTask = project.getTasks().getByName(LifecycleBasePlugin.ASSEMBLE_TASK_NAME);
-        assembleTask.dependsOn(supportJarTask);
+        assembleTask.dependsOn(supportConfiguration.getAllArtifacts().getBuildDependencies());
     }
 
 }
